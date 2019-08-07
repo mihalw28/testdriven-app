8ea31a14c3c94f6c630094128b1097baf32919a1
mihalw28 <michal.waszak@yahoo.com>
2019-08-07 12:12:57 +0200

    Add flask migrations.

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,16 +1,17 @@
 import os
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_debugtoolbar import DebugToolbarExtension
 from flask_cors import CORS
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-# import sys
 
 # instatinate extensions
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
 cors = CORS()
+migrate = Migrate()
 
 
 def create_app(script_info=None):
@@ -23,6 +24,7 @@ def create_app(script_info=None):
     db.init_app(app)
     toolbar.init_app(app)
     cors.init_app(app)
+    migrate.init_app(app, db)
 
     # register blueprints
     from project.api.users import users_blueprint
