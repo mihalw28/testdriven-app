59df071f35763146aa2ed231c692e8fa724c6981
mihalw28 <michal.waszak@yahoo.com>
2019-08-14 14:16:07 +0200

    Add utils.py to keep tests more DRY and update users.py with tests.

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request
 from flask_restful import Api, Resource
 from project import db
 from project.api.models import User
+from project.api.utils import authenticate_restful, is_admin
 from sqlalchemy import exc
 
 users_blueprint = Blueprint("users", __name__, template_folder="./templates")
@@ -37,9 +38,17 @@ class Users(Resource):
 
 
 class UsersList(Resource):
-    def post(self):
+
+    method_decorators = {"post": [authenticate_restful]}
+
+    def post(self, resp):
         post_data = request.get_json()
         response_object = {"status": "fail", "message": "Invalid payload."}
+        if not is_admin(resp):
+            response_object[
+                "message"
+            ] = "You do not have permission to do that."
+            return response_object, 401
         if not post_data:
             return response_object, 400
         username = post_data.get("username")
