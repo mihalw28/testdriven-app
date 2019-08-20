29f9a0cf98b5c36386375724fb30055499799df6
mihalw28 <michal.waszak@yahoo.com>
2019-08-20 14:23:56 +0200

    Modify test.sh.

--- test.sh
+++ test.sh
@@ -19,9 +19,9 @@ docker-compose run client npm run coverage
 inspect $? client
 docker-compose down
 
-# run e3e tests
+# run e2e tests
 docker-compose -f docker-compose-prod.yml up -d --build
-docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
+docker-compose -f docker-compose-prod.yml run users python manage.py recreate_db
 ./node_modules/.bin/cypress run --config baseUrl=http://localhost
 inspect $? e2e
 docker-compose -f docker-compose-prod.yml down
