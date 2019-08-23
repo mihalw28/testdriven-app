e45296e2fb0ac6dbe9c75eda10e688d18ab1537a
mihalw28 <michal.waszak@yahoo.com>
2019-08-23 16:34:34 +0200

    Add stage environment.

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -38,3 +38,9 @@ class ProductionConfig(BaseConfig):
 
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class StagingConfig(BaseConfig):
+    """Staging configuration"""
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
