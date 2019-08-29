6d76e5bd8014a057113a196454edd9a2cbb774a6
mihalw28 <michal.waszak@yahoo.com>
2019-08-29 13:47:28 +0200

    Change ECS cluster adress.

--- docker-push.sh
+++ docker-push.sh
@@ -5,7 +5,7 @@ then
 
   if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
     export DOCKER_ENV=stage
-    export REACT_APP_USERS_SERVICE_URL="testdriven-staging-alb-2074969383.us-east-1.elb.amazonaws.com"
+    export REACT_APP_USERS_SERVICE_URL="testdriven-staging-alb-1014464409.us-east-1.elb.amazonaws.com"
   elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
     export DOCKER_ENV=prod
   fi
