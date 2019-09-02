ca8b049630e1f15565d03d0fa54f83f0377a22ff
mihalw28 <michal.waszak@yahoo.com>
2019-09-02 19:16:39 +0200

    Update staging DNS url.

--- docker-push.sh
+++ docker-push.sh
@@ -5,7 +5,7 @@ then
 
   if [[ "$TRAVIS_BRANCH" == "staging" ]]; then
     export DOCKER_ENV=stage
-    export REACT_APP_USERS_SERVICE_URL="http://testdriven-staging-alb-1014464409.us-east-1.elb.amazonaws.com"
+    export REACT_APP_USERS_SERVICE_URL="http://testdriven-staging-alb-735455032.us-east-1.elb.amazonaws.com"
   elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
     export DOCKER_ENV=prod
   fi
