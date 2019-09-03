d1ca161ea6b82a6caf0f4d3a12e5a2bdbb2679e3
mihalw28 <michal.waszak@yahoo.com>
2019-09-03 09:11:31 +0200

    Update production build.

--- docker-push.sh
+++ docker-push.sh
@@ -8,6 +8,7 @@ then
     export REACT_APP_USERS_SERVICE_URL="http://testdriven-staging-alb-735455032.us-east-1.elb.amazonaws.com"
   elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
     export DOCKER_ENV=prod
+    export REACT_APP_USERS_SERVICE_URL="http://testdriven-production-alb-222827923.us-east-1.elb.amazonaws.com"
   fi
 
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
