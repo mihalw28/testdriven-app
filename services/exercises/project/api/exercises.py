2b99a117ef9279d9fdf0b392fc69f3e5b7b305af
mihalw28 <michal.waszak@yahoo.com>
2019-09-16 16:40:24 +0200

    Update production deployment environment.

--- services/exercises/project/api/exercises.py
+++ services/exercises/project/api/exercises.py
@@ -4,10 +4,19 @@ from project import db
 from project.api.models import Exercise
 from project.api.utils import authenticate_restful
 from sqlalchemy import exc
+from flask import Blueprint, request, jsonify
 
 
 exercises_blueprint = Blueprint("exercises", __name__)
 api = Api(exercises_blueprint)
+
+
+@exercises_blueprint.route("/exercises/ping", methods=["GET"])
+def ping_pong():
+    return jsonify({
+        "status": "success",
+        "message": "pong!"
+    })
 
 
 class ExerciseList(Resource):
